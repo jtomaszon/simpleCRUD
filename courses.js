4547faeff7e95816e8ae4e631c09a1112554e094
Javier Tomás Zon <jtomaszon@gmail.com>
2013-05-15 06:04:48 -0300

    Adding source for courses

---
+++ courses.js
@@ -0,0 +1,123 @@
+[
+	{
+		"_id" : ObjectId("51868dc69af2aadc3a6d2802"),
+		"code" : "APH001",
+		"name" : "BASICO DE PREVENÇÃO DE ACIDENTES DO TRABALHO",
+		"location" : "SRPVSP",
+		"courseID" : "4",
+		"dueDateSIAT" : "05.10.17",
+		"dueDateDECEA" : "05.25.17",
+		"startDate" : "06.25.17",
+		"endDate" : "06.29.17"
+	},
+	{
+		"_id" : ObjectId("51868dc69af2aadc3a6d2803"),
+		"code" : "ASE001",
+		"name" : "GERENCIAMENTO DE RECURSOS DE EQUIPE - TRM",
+		"location" : "DTCEAAF",
+		"courseID" : "32",
+		"dueDateSIAT" : "08.02.17",
+		"dueDateDECEA" : "08.17.17",
+		"startDate" : "09.18.17",
+		"endDate" : "09.20.17"
+	},
+	{
+		"_id" : ObjectId("51868dc69af2aadc3a6d2805"),
+		"code" : "ASE009",
+		"name" : "GERENCIAMENTO DO RISCO A SEGURANÇA OPERACIONAL NO SISCEAB",
+		"location" : "SRPVSP",
+		"courseID" : "3",
+		"dueDateSIAT" : "05.17.17",
+		"dueDateDECEA" : "06.01.17",
+		"startDate" : "07.02.17",
+		"endDate" : "07.06.17"
+	},
+	{
+		"_id" : ObjectId("51868dc69af2aadc3a6d2806"),
+		"code" : "ATM021",
+		"name" : "SISTEMA ANTICOLISÃO DE BORDO (ACAS II)",
+		"location" : "DTCEAGW",
+		"courseID" : "17",
+		"dueDateSIAT" : "05.17.17",
+		"dueDateDECEA" : "06.01.17",
+		"startDate" : "07.02.17",
+		"endDate" : "07.03.17"
+	},
+	{
+		"_id" : ObjectId("51868dc69af2aadc3a6d280d"),
+		"code" : "CTP001",
+		"name" : "PADRONIZAÇÃO DA INSTRUÇÃO NO SISCEAB",
+		"location" : "SRPVSP",
+		"courseID" : "8",
+		"dueDateSIAT" : "07.05.17",
+		"dueDateDECEA" : "07.20.17",
+		"startDate" : "08.20.17",
+		"endDate" : "08.31.17"
+	},
+	{
+		"_id" : ObjectId("51868dc69af2aadc3a6d280f"),
+		"code" : "CTP004",
+		"name" : "FRASEOLOGIA ATC PARA SITUAÇÕES DE EMERGÊNCIA",
+		"location" : "DTCEAGW",
+		"courseID" : "1",
+		"dueDateSIAT" : "12.21.16",
+		"dueDateDECEA" : "01.05.17",
+		"startDate" : "02.05.17",
+		"endDate" : "02.09.17"
+	},
+	{
+		"_id" : ObjectId("51868dc69af2aadc3a6d2815"),
+		"code" : "CTP006",
+		"name" : "CAPACITAÇÃO DE INSTRUÇÃO PRÁTICA OPERACIONAL",
+		"location" : "DTCEASP",
+		"courseID" : "14",
+		"dueDateSIAT" : "07.26.17",
+		"dueDateDECEA" : "08.10.17",
+		"startDate" : "09.10.17",
+		"endDate" : "09.21.17"
+	},
+	{
+		"_id" : ObjectId("51868dc69af2aadc3a6d2817"),
+		"code" : "CTP010",
+		"name" : "INGLÊS ATC",
+		"location" : "SRPVSP",
+		"courseID" : "5",
+		"dueDateSIAT" : "01.25.17",
+		"dueDateDECEA" : "02.09.17",
+		"startDate" : "03.12.17",
+		"endDate" : "03.30.17"
+	},
+	{
+		"_id" : ObjectId("51868dc69af2aadc3a6d281b"),
+		"code" : "NAV012",
+		"name" : "MANUTENÇÃO DE GERADOR DE HIDROGÊNIO STUART M-16",
+		"location" : "DTCEAGW",
+		"courseID" : "1",
+		"dueDateSIAT" : "10.11.17",
+		"dueDateDECEA" : "10.26.17",
+		"startDate" : "11.26.17",
+		"endDate" : "12.14.17"
+	},
+	{
+		"_id" : ObjectId("51868dc69af2aadc3a6d281c"),
+		"code" : "NAV017",
+		"name" : "MANUTENÇÃO DA ESTAÇÃO METEOROLOGICA DE ALTITUDE DIGICORA III",
+		"location" : "DTCEAGW",
+		"courseID" : "1",
+		"dueDateSIAT" : "06.21.17",
+		"dueDateDECEA" : "07.06.17",
+		"startDate" : "08.06.17",
+		"endDate" : "08.24.17"
+	},
+	{
+		"_id" : ObjectId("51868dc69af2aadc3a6d281d"),
+		"code" : "SAR005",
+		"name" : "BÁSICO DE BUSCA E SALVAMENTO",
+		"location" : "SRPVSP",
+		"courseID" : "9",
+		"dueDateSIAT" : "06.28.17",
+		"dueDateDECEA" : "07.13.17",
+		"startDate" : "08.13.17",
+		"endDate" : "08.17.17"
+	}
+]
